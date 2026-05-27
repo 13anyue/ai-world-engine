46af5cc92f56d4fc64c2b29e0957dd392de66ebd
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:25:31 +0800

    Update worldSystem.js

--- systems/worldSystem.js
+++ systems/worldSystem.js
@@ -1,4 +1,9 @@
-import { sendAIMessage }
+import {
+
+sendAIMessage
+
+}
+
 from "./apiSystem.js";
 
 export async function generateStory(){
@@ -14,17 +19,13 @@ content:`
 
 你是高级AI文游引擎。
 
-你需要：
-
 生成沉浸式剧情。
 
-规则：
+对白自然。
 
-1 对白使用自然风格
-2 内心戏使用细腻描写
-3 剧情有电影感
-4 保持世界真实性
-5 输出1000字以内
+内心戏细腻。
+
+1000字以内。
 
 `
 
