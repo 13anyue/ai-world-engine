d97366db5c92a5b2842cb424d17923a4076d4813
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:24:48 +0800

    Update createPage.js

--- pages/createPage.js
+++ pages/createPage.js
@@ -4,32 +4,31 @@ return `
 
 <div class="card">
 
-    <div class="title">
-        创建世界
-    </div>
+<div class="title">
+创建世界
+</div>
 
-    <div class="sub">
+<div class="sub">
 
-        输入世界背景，
-        AI会生成完整世界。
+输入世界背景。
 
-    </div>
+</div>
 
-    <input
-    class="input"
-    placeholder="世界名称">
+<input
+class="input"
+placeholder="世界名称">
 
-    <textarea
-    class="input"
-    placeholder="输入世界设定"></textarea>
+<textarea
+class="input"
+placeholder="输入世界设定"></textarea>
 
-    <button
-    class="button"
-    onclick="window.enterStory()">
+<button
+class="button"
+onclick="window.enterStory()">
 
-        进入世界
+进入世界
 
-    </button>
+</button>
 
 </div>
 
