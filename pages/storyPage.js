5e9c1fe774836c93310bd6ba3af7f953337b1be2
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:09:09 +0800

    Update storyPage.js

--- pages/storyPage.js
+++ pages/storyPage.js
@@ -1,1 +1,43 @@
+export function renderStoryPage(){
 
+return `
+
+<div class="card">
+
+    <div class="title">
+        雨夜都市
+    </div>
+
+    <div class="sub">
+
+        21:44 · 暴雨
+
+    </div>
+
+</div>
+
+<div class="card">
+
+    <div class="story">
+
+        <b>沈辞：</b>
+
+        “你终于来了。”
+
+        <br><br>
+
+        <i>
+        空气里弥漫着潮湿气息。
+        </i>
+
+        <br><br>
+
+        桌上放着一份旧档案袋。
+
+    </div>
+
+</div>
+
+`;
+
+}
