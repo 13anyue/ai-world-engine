83bbbd23f1b518268e66471aba60c707c69731ce
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:55:58 +0800

    Update app.js

--- core/app.js
+++ core/app.js
@@ -3,6 +3,14 @@ from "./router.js";
 
 import {
 
+createNPC
+
+}
+
+from "../systems/npcSystem.js";
+
+import {
+
 saveAPIData
 
 }
@@ -46,5 +54,43 @@ window.nextStory = ()=>{
 loadStory();
 
 };
+
+window.openRelation = ()=>{
+
+navigate("relation");
+
+};
+
+createNPC({
+
+name:"沈辞",
+
+age:24,
+
+gender:"男",
+
+personality:"冷淡腹黑",
+
+occupation:"财团继承人",
+
+mood:"复杂"
+
+});
+
+createNPC({
+
+name:"林镜",
+
+age:22,
+
+gender:"女",
+
+personality:"病娇",
+
+occupation:"艺术学院学生",
+
+mood:"愉悦"
+
+});
 
 navigate("home");
