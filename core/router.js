8471337775fdb9c259ccd01a116af5bb218ea9ad
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:06:26 +0800

    Update router.js

--- core/router.js
+++ core/router.js
@@ -1,1 +1,40 @@
+import { renderHomePage }
+from "../pages/homePage.js";
 
+import { renderCreatePage }
+from "../pages/createPage.js";
+
+import { renderStoryPage }
+from "../pages/storyPage.js";
+
+const pageContainer =
+document.getElementById("page-container");
+
+export function navigate(page){
+
+switch(page){
+
+case "home":
+
+pageContainer.innerHTML =
+renderHomePage();
+
+break;
+
+case "create":
+
+pageContainer.innerHTML =
+renderCreatePage();
+
+break;
+
+case "story":
+
+pageContainer.innerHTML =
+renderStoryPage();
+
+break;
+
+}
+
+}
