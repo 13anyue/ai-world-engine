55646e9c19a8a6664892e5941c7c4f2886f4db7a
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:07:19 +0800

    Update homePage.js

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,1 +1,29 @@
+export function renderHomePage(){
 
+return `
+
+<div class="card">
+
+    <div class="title">
+        AI互动文游平台
+    </div>
+
+    <div class="sub">
+
+        创建属于你的动态AI世界。
+
+    </div>
+
+    <button
+    class="button"
+    onclick="window.navigateToCreate()">
+
+        创建游戏
+
+    </button>
+
+</div>
+
+`;
+
+}
