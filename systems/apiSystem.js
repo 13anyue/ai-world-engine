7bac6aa0a3ea838137caed940035d5aaf0f8a6ab
13anyue <xiaoshayang9@gmail.com>
2026-05-27 11:12:05 +0800

    Update apiSystem.js

--- systems/apiSystem.js
+++ systems/apiSystem.js
@@ -1,1 +1,57 @@
+import { GAME_STATE }
+from "../core/state.js";
 
+/* =========================
+发送AI请求
+========================= */
+
+export async function sendAIMessage(messages){
+
+try{
+
+const response = await fetch(
+
+GAME_STATE.api.url,
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json",
+
+"Authorization":
+`Bearer ${GAME_STATE.api.key}`
+
+},
+
+body:JSON.stringify({
+
+model:GAME_STATE.api.model,
+
+messages
+
+})
+
+}
+
+);
+
+const data =
+await response.json();
+
+return data
+?.choices?.[0]
+?.message
+?.content || "AI无响应";
+
+}catch(error){
+
+console.log(error);
+
+return "请求失败";
+
+}
+
+}
